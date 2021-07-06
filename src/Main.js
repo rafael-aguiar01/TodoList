8e6a05746f64bf6887fd4ab830e25b7355da8aca
Rafael <rafael.a1@hotmail.com>
2021-07-06 12:06:20 -0300

    Criando layout da aplicação

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <div>
+            <div className="row justify-content-center">
+                <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
+                    <h1>To Do List</h1>
+                    <input type="text" placeholder="Enter task" className='form-control'/>
+                    <button className='btn btn-success' > ADD </button>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
